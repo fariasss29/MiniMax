f6a21ee1c8d512d35eebcafcabb3e0c6e6174aeb
Kauã Kouqui <144644564+Kouqui@users.noreply.github.com>
2026-03-25 23:02:37 +0000

    fix: removendo comentários

--- minimax.py
+++ minimax.py
@@ -5,15 +5,6 @@ import platform
 import time
 from os import system
 
-"""
-An implementation of Minimax AI Algorithm in Tic Tac Toe,
-using Python.
-This software is available under GPL license.
-Author: Clederson Cruz
-Year: 2017
-License: GNU GENERAL PUBLIC LICENSE (GPL)
-"""
-
 HUMAN = -1
 COMP = +1
 board = [
@@ -24,11 +15,6 @@ board = [
 
 
 def evaluate(state):
-    """
-    Function to heuristic evaluation of state.
-    :param state: the state of the current board
-    :return: +1 if the computer wins; -1 if the human wins; 0 draw
-    """
     if wins(state, COMP):
         score = +1
     elif wins(state, HUMAN):
@@ -40,15 +26,6 @@ def evaluate(state):
 
 
 def wins(state, player):
-    """
-    This function tests if a specific player wins. Possibilities:
-    * Three rows    [X X X] or [O O O]
-    * Three cols    [X X X] or [O O O]
-    * Two diagonals [X X X] or [O O O]
-    :param state: the state of the current board
-    :param player: a human or a computer
-    :return: True if the player wins
-    """
     win_state = [
         [state[0][0], state[0][1], state[0][2]],
         [state[1][0], state[1][1], state[1][2]],
@@ -66,20 +43,10 @@ def wins(state, player):
 
 
 def game_over(state):
-    """
-    This function test if the human or computer wins
-    :param state: the state of the current board
-    :return: True if the human or computer wins
-    """
     return wins(state, HUMAN) or wins(state, COMP)
 
 
 def empty_cells(state):
-    """
-    Each empty cell will be added into cells' list
-    :param state: the state of the current board
-    :return: a list of empty cells
-    """
     cells = []
 
     for x, row in enumerate(state):
@@ -91,12 +58,6 @@ def empty_cells(state):
 
 
 def valid_move(x, y):
-    """
-    A move is valid if the chosen cell is empty
-    :param x: X coordinate
-    :param y: Y coordinate
-    :return: True if the board[x][y] is empty
-    """
     if [x, y] in empty_cells(board):
         return True
     else:
@@ -104,12 +65,6 @@ def valid_move(x, y):
 
 
 def set_move(x, y, player):
-    """
-    Set the move on board, if the coordinates are valid
-    :param x: X coordinate
-    :param y: Y coordinate
-    :param player: the current player
-    """
     if valid_move(x, y):
         board[x][y] = player
         return True
@@ -118,14 +73,6 @@ def set_move(x, y, player):
 
 
 def minimax(state, depth, player):
-    """
-    AI function that choice the best move
-    :param state: current state of the board
-    :param depth: node index in the tree (0 <= depth <= 9),
-    but never nine in this case (see iaturn() function)
-    :param player: an human or a computer
-    :return: a list with [the best row, best col, best score]
-    """
     if player == COMP:
         best = [-1, -1, -infinity]
     else:
@@ -153,9 +100,6 @@ def minimax(state, depth, player):
 
 
 def clean():
-    """
-    Clears the console
-    """
     os_name = platform.system().lower()
     if 'windows' in os_name:
         system('cls')
@@ -164,11 +108,6 @@ def clean():
 
 
 def render(state, c_choice, h_choice):
-    """
-    Print the board on console
-    :param state: current state of the board
-    """
-
     chars = {
         -1: h_choice,
         +1: c_choice,
@@ -185,13 +124,6 @@ def render(state, c_choice, h_choice):
 
 
 def ai_turn(c_choice, h_choice):
-    """
-    It calls the minimax function if the depth < 9,
-    else it choices a random coordinate.
-    :param c_choice: computer's choice X or O
-    :param h_choice: human's choice X or O
-    :return:
-    """
     depth = len(empty_cells(board))
     if depth == 0 or game_over(board):
         return
@@ -212,12 +144,6 @@ def ai_turn(c_choice, h_choice):
 
 
 def human_turn(c_choice, h_choice):
-    """
-    The Human plays choosing a valid move.
-    :param c_choice: computer's choice X or O
-    :param h_choice: human's choice X or O
-    :return:
-    """
     depth = len(empty_cells(board))
     if depth == 0 or game_over(board):
         return
@@ -251,9 +177,6 @@ def human_turn(c_choice, h_choice):
 
 
 def main():
-    """
-    Main function that calls all functions
-    """
     clean()
     h_choice = ''  # X or O
     c_choice = ''  # X or O
